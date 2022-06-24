385f9c9a6a261e18a734a15f7a93b035a7579fbb
Tim <sa200602687@yahoo.com>
2022-06-24 10:29:29 +0800

    arduino

--- Task7/Task7.ino
+++ Task7/Task7.ino
@@ -1,20 +1,23 @@
 #define AIN1 6
 #define AIN2 5
-
-
+#define volt1 0
+int i = 0;
 void setup() {
   Serial.begin(9600);
-  
-  pinMode(AIN1,OUTPUT);
   pinMode(AIN2,OUTPUT);
+  pinMode(AIN1,OUTPUT);
   
 }
  
 void loop() {
-  
-  digitalWrite(AIN1,HIGH); 
-  digitalWrite(AIN2,LOW);
-
-  delay(1000);
-
-}
+  digitalWrite(AIN2, LOW);
+  for(int i = 0; i<=255; i++);
+    analogWrite(AIN1,i); 
+    delay(100);
+      if(volt1 == 255){
+        for(int i = 255; i>0; i--);
+          analogWrite(AIN1,i);
+          delay(100);
+    }
+  }
+ 
